7f1cc4526cd795b17bb027e8f920b2bed3d47e36
KenWood2b <kenwood2b@gmail.com>
2024-11-03 14:30:34 +0300

    First

---
+++ HelloWorldConsoleApp/HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Привет мир!!!");
+            Write("Привет");
+            Write(" мир");
+            Write("!!!");
+
+            ReadKey();
+        }
+    }
+}
